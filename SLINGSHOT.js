bf81b8b46df7ae1107e01dc768a4175f4e7ead98
Yash-Chauhan14 <krunalvchauhan@gmail.com>
2020-11-30 17:01:42 +0530

    commit

---
+++ SLINGSHOT.js
@@ -0,0 +1,24 @@
+class SLINGSHOT{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+        this.point = pointB
+    }
+      fly(){
+          this.sling.bodyA = null
+      }
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        
+        strokeWeight(4);
+        line(pointA.x, pointA.y, this.point.x, this.point.y);
+    }}
+    
+}
